f237961a6a61eb4d57baa0d48173550783fd2bcf
Paul Tan <pjxtan@gmail.com>
2014-09-02 17:34:20 -0400

    K&R ch. 1, ex. 10

---
+++ knr_exs/knr_1-10.c
@@ -0,0 +1,28 @@
+/* K&R Exercise 1-10 */
+
+#include <stdio.h>
+
+int main() {
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            putchar('\\');
+            putchar('t');
+        }
+        else if (c == ' ') {
+            putchar('\\');
+            putchar('b');
+        }
+        else if (c == '\\') {
+            putchar('\\');
+            putchar('\\');
+        }
+        else
+            putchar(c);
+    }
+
+    return 0;
+}
+
+
