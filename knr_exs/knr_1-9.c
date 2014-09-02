2354facce6bca933e9f9dcea0793ee54f9e1b569
Paul Tan <pjxtan@gmail.com>
2014-09-02 17:34:13 -0400

    K&R ch. 1, ex. 9

---
+++ knr_exs/knr_1-9.c
@@ -0,0 +1,21 @@
+/* K&R Exercise 1-9 */
+
+#include <stdio.h>
+
+int main() {
+    int c;
+    int prev_was_blank = 0;
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ')
+            prev_was_blank = 1;
+        else if (prev_was_blank) {
+            putchar(' ');
+            putchar(c);
+            prev_was_blank = 0;
+        }
+    }
+    return 0;
+}
+
+
