59ea73de300eb012f7627b2c7243fadfb7ff9914
Paul Tan <pjxtan@gmail.com>
2014-09-02 17:34:04 -0400

    K&R ch. 1, ex. 8

---
+++ knr_exs/knr_1-8.c
@@ -0,0 +1,24 @@
+/* K&R Exercise 1-8 */
+
+#include <stdio.h>
+
+int main() {
+    int c;
+    int nb = 0;    /* blanks (presumably whitespace?) */
+    int nt = 0;    /* tabs */
+    int nl = 0;    /* newlines */
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ')
+            ++nb;
+        else if (c == '\t')
+            ++nt;
+        else if (c == '\n')
+            ++nl;
+    }
+    printf("Blanks:   %d\n", nb);
+    printf("Tabs:     %d\n", nt);
+    printf("Newlines: %d\n", nl);
+
+    return 0;
+}
