ead5e064c7dec361047dcafc547510c66a547791
Paul Tan <pjxtan@gmail.com>
2014-09-22 18:54:26 -0400

    Added names and more colours

--- utils/colours.bash
+++ utils/colours.bash
@@ -2,10 +2,25 @@
 
 # A fun reference for Bash colour codes
 
+echo " BG  black   red     green   yellow  blue    magenta cyan    lgray"
 for i in {40..47}; do
     echo -en " ${i} "
     for j in {30..37}; do
-        echo -en "\033[${i};${j}m ${j} "
+        echo -en "\033[${i};${j}m ${j}     "
+    # turn off colour
+    echo -en "\033[0m"
+    done
+    echo
+done
+
+echo
+
+echo " BG   dgray    lred     lgreen   lyellow  lblue    lmagenta lcyan    white"
+for i in {100..107}; do
+    echo -en " ${i} "
+    for j in {90..97}; do
+        echo -en "\033[${i};${j}m ${j}      "
+    # turn off colour
     echo -en "\033[0m"
     done
     echo
