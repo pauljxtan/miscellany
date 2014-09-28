ac88b60cb8f31070563e71473f7d47b347c4b688
Paul Tan <pjxtan@gmail.com>
2014-09-28 14:47:39 -0400

    K&R Exercise 1-18

---
+++ knr_exs/knr_1-18.c
@@ -0,0 +1,39 @@
+/* K&R Exercise 1-18
+ * Write a program to remove trailing blanks and tabs from each line of input,
+ * and to delete entirely blank lines. */
+
+#include <stdio.h>
+
+static const int MAX_LINE_LEN = 1000;
+
+// Reads a line into s and returns the length.
+int getline1(char s[]) {
+    int c, i;
+
+    for (i = 0; i < MAX_LINE_LEN - 1 && (c = getchar()) != EOF && c != '\n';
+         i++)
+        s[i] = c;
+    if (c == '\n')
+        s[i++] = c;
+    
+    return i;
+}
+
+int main() {
+    int i, j, strlen;
+    char line[MAX_LINE_LEN]; // stores each line
+
+    while ((strlen = getline1(line)) > 0) {
+        i = strlen - 2;
+        // i stops decrementing at beginning of trailing spaces/tabs
+        while (line[i] == ' ' || line[i] == '\t')
+            i--;
+
+        // print line
+        for (j = 0; j < i + 1; j++)
+            putchar(line[j]);
+        putchar('\n');
+    }
+
+    return 0;
+}
