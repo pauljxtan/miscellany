03cde21da0b679403f6b364f8339333023246777
Paul Tan <pjxtan@gmail.com>
2014-09-28 14:11:56 -0400

    Replaced maximum word length magic number with constant

--- knr_exs/knr_1-13.c
+++ knr_exs/knr_1-13.c
@@ -1,18 +1,22 @@
 /* K&R Exercise 1-13
  * Write a program to print a histogram of the lengths of words in its input.
  * It is easy to draw the histogram with the bars horizontal; a vertical
- * orientation is more challenging. */
+ * orientation is more challenging. 
+ *
+ * Note: The maximum word length is fixed at MAX_WORD_LENGTH. */
 
 #include <stdio.h>
+
+static const int MAX_WORD_LENGTH = 10;
 
 int main() {
     int c;
     int i;
     int j;
-    int counts[10];
+    int counts[MAX_WORD_LENGTH + 1];
 
     // Initialize the counts to zero
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < MAX_WORD_LENGTH + 1; i++)
         counts[i] = 0;
 
     i = 0;
@@ -26,7 +30,7 @@ int main() {
     }
 
     // Print the histogram
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < MAX_WORD_LENGTH + 1; i++) {
         printf("%d: ", i);
         for (j = 0; j < counts[i]; j++)
             printf("|");
