6aee68e0dc493b5129eec4fed6870ec770dc198b
Paul J.X. Tan <pjxtan@gmail.com>
2015-09-17 13:11:46 -0400

    Extended base10_to_basen() up to base-36

--- algorithms/python/convert.py
+++ algorithms/python/convert.py
@@ -1,21 +1,29 @@
+SYMBOLS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c',
+           'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+           'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 def base10_to_basen(x, n):
     """
-    Converts a number from base-10 (decimal) to base-n, where 2 <= n <= 9.
+    Converts a number from base-10 (decimal) to base-n, where 1 <= n <= 36.
 
     @type  x: integer
     @param x: number in base-10
     @type  n: integer
-    @param n: base to convert to
+    @param n: base to convert to (1 <= n <= 36)
     
     @rtype: string
     @return: number in base-n
     """
     result = ""
 
+    # Base-1 is trivial
+    if n == 1:
+        return '1' * x
+
     while x > 0:
         # Prepend remainder to result
-        result = str(x % n) + result
-        # Move to next position
+        result = SYMBOLS[x % n] + result
+        # Move to next digit
         x /= n
 
     return result
