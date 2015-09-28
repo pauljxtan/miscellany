d8d2d1eaf687169e2d8d0f076bb29c3049471d96
Paul J.X. Tan <pjxtan@gmail.com>
2015-09-28 15:32:27 -0400

    Use lexical scoping in (cbrt)

--- sicp_exs/sicp_1-8.scm
+++ sicp_exs/sicp_1-8.scm
@@ -14,23 +14,27 @@
   (* x x x))
 
 (define (cbrt x)
-  (define (good-enough? guess x)
+  
+  (define (good-enough? guess)
     (< (abs (- (cube guess) 
                x))
        0.000001))
+  
   ; (x/y^2 + 2*y) / 3
-  (define (improve guess x)
+  (define (improve guess)
     (average guess
              (/ (+ (/ x 
                       (square guess)) 
                    (* 2 guess))
                 3)))
-  (define (cbrt-iter guess x)
-    (if (good-enough? guess x)
+  
+  (define (cbrt-iter guess)
+    (if (good-enough? guess)
       guess
-      (cbrt-iter (improve guess x) 
-                 x)))
-  (cbrt-iter 1.0 x))
+      (cbrt-iter (improve guess))))
+
+  (cbrt-iter 1.0)
+)
 
 (format #t "cbrt(8)   = ~f\n" (cbrt 8))
 (format #t "cbrt(27)  = ~f\n" (cbrt 27))
