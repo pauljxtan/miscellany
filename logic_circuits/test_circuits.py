b87f779b0e15396761a3fb089e541f4c0dbbfb08
Paul J.X. Tan <pjxtan@gmail.com>
2015-10-23 14:28:06 -0400

    Added tests for 2:1 and 4:1 multiplexers

--- logic_circuits/test_circuits.py
+++ logic_circuits/test_circuits.py
@@ -1,3 +1,4 @@
+import itertools
 import unittest
 
 import circuits
@@ -19,6 +20,32 @@ class Tests(unittest.TestCase):
         self.assertEqual(circuits.full_adder(1, 0, 1), (1, 0))
         self.assertEqual(circuits.full_adder(0, 1, 1), (1, 0))
         self.assertEqual(circuits.full_adder(1, 1, 1), (1, 1))
+
+    def test_multiplexer_2to1(self):
+        for (S, A, B) in itertools.product((0, 1), repeat=3):
+            if S == 0:
+                self.assertEqual(circuits.multiplexer_2to1(S, A, B), A)
+            else:
+                self.assertEqual(circuits.multiplexer_2to1(S, A, B), B)
+
+        # Test enable=0 (active high)
+        for (S, A, B) in itertools.product((0, 1), repeat=3):
+            self.assertEqual(circuits.multiplexer_2to1(S, A, B, 0), 0)
+
+    def test_multiplexer_4to1(self):
+        for (S_0, S_1, A, B, C, D) in itertools.product((0, 1), repeat=6):
+            if S_1 == 0 and S_0 == 0:
+                self.assertEqual(circuits.multiplexer_4to1(S_0, S_1, A, B, C, D), A)
+            elif S_1 == 0 and S_0 == 1:
+                self.assertEqual(circuits.multiplexer_4to1(S_0, S_1, A, B, C, D), B)
+            elif S_1 == 1 and S_0 == 0:
+                self.assertEqual(circuits.multiplexer_4to1(S_0, S_1, A, B, C, D), C)
+            else:
+                self.assertEqual(circuits.multiplexer_4to1(S_0, S_1, A, B, C, D), D)
+
+        # Test enable=0 (active high)
+        for (S_0, S_1, A, B, C, D) in itertools.product((0, 1), repeat=6):
+            self.assertEqual(circuits.multiplexer_4to1(S_0, S_1, A, B, C, D, 0), 0)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
